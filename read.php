082157871a5fa2ca3f059b14127cea7152448b39
Hidayatul Husna <hidayatulhusna618@gmail.com>
2024-06-09 16:33:34 +0800

    second commit

--- read.php
+++ read.php
@@ -17,7 +17,7 @@ $result = $user->getUsers();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Q5</title>
+    <title>Read</title>
     <style>
         body{
         font-family: 'Arial', sans-serif;
